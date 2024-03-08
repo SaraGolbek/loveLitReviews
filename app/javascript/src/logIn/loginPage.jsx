7252766c76e6c029f9697772378bcd00c49cdd57
SaraGolbek <145944090+SaraGolbek@users.noreply.github.com>
2024-03-08 11:21:03 -0800

    Update loginPage.jsx

--- app/javascript/src/logIn/loginPage.jsx
+++ app/javascript/src/logIn/loginPage.jsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import Layout from './layout';
 import SignInWidget from './signInWidget';
 import SignUpWidget from './signUpWidget';
 import './stylesheets/login';
 
-const LogIn = () => {
+const LogInPage = () => {
+  const [displaySignIn, setDisplaySignIn] = useState(true);
+  const [displaySignUp, setDisplaySignUp] = useState(false);
+
+  const toggleDisplayProperties = () => {
+    setDisplaySignIn(true);
+    setDisplaySignUp(false);
+  };
+
+  const toggleDisplayBookings = () => {
+    setDisplaySignIn(false);
+    setDisplaySignUp(true);
+  };
   
   return (
     <Layout>
@@ -15,7 +27,8 @@ const LogIn = () => {
           <p>Join our passionate community of romance enthusiasts! Dive into the world of love, intrigue, and excitement with our unique platform where readers share their thoughts on the latest page-turners. Whether you're seeking heart-pounding tales or sizzling encounters, our users rate books based on overall enjoyment, captivating storytelling, writing style, and the delicious 'spiciness' factor. Join us and discover your next obsession in the realm of romance!</p>
         </div>
         <div className="col-4 mt-5">
-          
+          {displaySignIn && <SignInWidget />}
+          {displaySignUp && <SignUpWidget />}
         </div>
       </div>
     </Layout>
@@ -24,7 +37,7 @@ const LogIn = () => {
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
-    <LogIn />,
+    <LogInPage />,
     document.body.appendChild(document.createElement('div')),
   )
 })
