46b8f1d28ef9edb558dd356f00845b96b7fa210e
SaraGolbek <145944090+SaraGolbek@users.noreply.github.com>
2024-03-08 10:53:44 -0800

    Update signInWidget.jsx

--- app/javascript/src/logIn/signInWidget.jsx
+++ app/javascript/src/logIn/signInWidget.jsx
@@ -1,9 +1,26 @@
-<form className="log-in" onSubmit={handleLogIn}>
-  <h6 className="mb-3">Welcome Back!</h6>
-  <input type="text"  className="inputwidth username" id="usernameinput" placeholder="Username" required onChange={(event) => userLogIn(event.target.value)}></input>
-  <input type="password" className="password" id="passinput" placeholder="Password" required onChange={(event) => passwordLogIn(event.target.value)}></input>
-  <button id="log-in-btn">Sign In</button>
-  <label htmlFor="remember"><input type="checkbox" id="remember" className="me-1"></input>
-  Remember me</label>
-  <a href="#" className="ms-3">Forgot Password?</a>
-</form>
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './stylesheets/login';
+
+const signInWidget = () => {
+  
+  return (
+    <form className="log-in" onSubmit={handleLogIn}>
+      <h6 className="mb-3">Welcome Back!</h6>
+      <input type="text"  className="inputwidth username" id="usernameinput" placeholder="Username" required onChange={(event) => userLogIn(event.target.value)}></input>
+      <input type="password" className="password" id="passinput" placeholder="Password" required onChange={(event) => passwordLogIn(event.target.value)}></input>
+      <button id="log-in-btn">Sign In</button>
+      <label htmlFor="remember"><input type="checkbox" id="remember" className="me-1"></input>
+      Remember me</label>
+      <a href="#" className="ms-3">Forgot Password?</a>
+    </form>
+ );
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  ReactDOM.render(
+    <signInWidget />,
+    document.body.appendChild(document.createElement('div')),
+  )
+})
