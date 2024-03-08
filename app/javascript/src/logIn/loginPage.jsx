9edef12bce8ec561494c0adace5e8f5e2dd90129
SaraGolbek <145944090+SaraGolbek@users.noreply.github.com>
2024-03-08 10:01:17 -0800

    Update loginPage.jsx

--- app/javascript/src/logIn/loginPage.jsx
+++ app/javascript/src/logIn/loginPage.jsx
@@ -10,7 +10,7 @@ const LogIn = () => {
       <div className="row">
         <div className="col-5 mt-5 offset-2" id="hero">
           <h3 id="welcome">Welcome to LoveLitReviews</h3>
-          <p>Welcome to our passionate community of romance enthusiasts! Dive into the world of love, intrigue, and excitement with our unique platform where readers share their thoughts on the latest page-turners. Whether you're seeking heart-pounding tales or sizzling encounters, our users rate books based on overall enjoyment, captivating storytelling, writing style, and the delicious 'spiciness' factor. Join us and discover your next obsession in the realm of romance!</p>
+          <p>Join our passionate community of romance enthusiasts! Dive into the world of love, intrigue, and excitement with our unique platform where readers share their thoughts on the latest page-turners. Whether you're seeking heart-pounding tales or sizzling encounters, our users rate books based on overall enjoyment, captivating storytelling, writing style, and the delicious 'spiciness' factor. Join us and discover your next obsession in the realm of romance!</p>
         </div>
         <div className="col-4 mt-5">
           <div className="row border shadow rounded" id="acc">
