1a13876631aa4d14634c5afece214009f2b34a67
SaraGolbek <145944090+SaraGolbek@users.noreply.github.com>
2024-03-08 11:28:16 -0800

    Update signUpWidget.jsx

--- app/javascript/src/logIn/signUpWidget.jsx
+++ app/javascript/src/logIn/signUpWidget.jsx
@@ -4,16 +4,20 @@ import ReactDOM from 'react-dom';
 import './stylesheets/login';
 
 class SignUpWidget extends React.Component {
-  
- return ( 
-   <form className="sign-up" onSubmit={handleSignUp}>
-     <h6 className="mb-3">New to Twitter?</h6>
-     <input type="text" className="inputwidth email" id="newemail" placeholder="Email" required onChange={(event) => setEmail(event.target.value)}></input>
-     <input type="text" className="inputwidth username" id="newusername" placeholder="Username" required onChange={(event) => setUsername(event.target.value)}></input>
-     <input type="password" className="inputwidth password" id="newpass" placeholder="Password" required onChange={(event) => setPassword(event.target.value)}></input>
-     <button className="inputwidth" id="sign-up-btn">Sign Up for Twitter</button>
-   </form>
-   );
+
+  render() {
+   return ( 
+     <React.Fragment>
+       <form className="sign-up" onSubmit={handleSignUp}>
+         <h6 className="mb-3">New to Twitter?</h6>
+         <input type="text" className="inputwidth email" id="newemail" placeholder="Email" required onChange={(event) => setEmail(event.target.value)}></input>
+         <input type="text" className="inputwidth username" id="newusername" placeholder="Username" required onChange={(event) => setUsername(event.target.value)}></input>
+         <input type="password" className="inputwidth password" id="newpass" placeholder="Password" required onChange={(event) => setPassword(event.target.value)}></input>
+         <button className="inputwidth" id="sign-up-btn">Sign Up for Twitter</button>
+       </form>
+     </React.Fragment>
+     );
+  }
 }
 
 export defualt SignUpWidget;
