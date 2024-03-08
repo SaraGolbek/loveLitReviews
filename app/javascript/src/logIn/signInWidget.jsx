1982791ce2786da1ff8b35761ba2958b964198df
SaraGolbek <145944090+SaraGolbek@users.noreply.github.com>
2024-03-08 11:43:47 -0800

    Update signInWidget.jsx

--- app/javascript/src/logIn/signInWidget.jsx
+++ app/javascript/src/logIn/signInWidget.jsx
@@ -8,14 +8,12 @@ class SignInWidget extends React.Component {
   render() {
     return (
       <React.Fragment>
-        <form className="log-in" onSubmit={handleLogIn}>
-          <h6 className="mb-3">Welcome Back!</h6>
-          <input type="text"  className="inputwidth username" id="usernameinput" placeholder="Username" required onChange={(event) => userLogIn(event.target.value)}></input>
-          <input type="password" className="password" id="passinput" placeholder="Password" required onChange={(event) => passwordLogIn(event.target.value)}></input>
+        <form className="log-in">
+          <h6 className="mb-3">Sign In</h6>
+          <input type="text"  className="username" id="usernameinput" placeholder="Username" ></input>
+          <input type="password" className="password" id="passinput" placeholder="Password" ></input>
+          <p>No Account? Sign Up</p>
           <button id="log-in-btn">Sign In</button>
-          <label htmlFor="remember"><input type="checkbox" id="remember" className="me-1"></input>
-          Remember me</label>
-          <a href="#" className="ms-3">Forgot Password?</a>
         </form>
       </React.Fragment>
    );
