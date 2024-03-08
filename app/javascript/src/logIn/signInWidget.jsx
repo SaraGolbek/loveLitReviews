9aa100871cd803b53ea591233fb3994304423423
SaraGolbek <145944090+SaraGolbek@users.noreply.github.com>
2024-03-08 13:03:22 -0800

    Update signInWidget.jsx

--- app/javascript/src/logIn/signInWidget.jsx
+++ app/javascript/src/logIn/signInWidget.jsx
@@ -1,23 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-
 import './stylesheets/login';
 
 class SignInWidget extends React.Component {
-
   render() {
     return (
       <React.Fragment>
         <form className="log-in">
           <h6 className="mb-3">Sign In</h6>
-          <input type="text"  className="username" id="usernameinput" placeholder="Username" ></input>
-          <input type="password" className="password" id="passinput" placeholder="Password" ></input>
-          <p>No Account? Sign Up</p>
+          <input type="text" className="username" id="usernameinput" placeholder="Username"></input>
+          <input type="password" className="password" id="passinput" placeholder="Password"></input>
+          <p onClick={this.props.onSignUpClick}>No Account? Sign Up</p>
           <button id="log-in-btn">Sign In</button>
         </form>
       </React.Fragment>
-   );
+    );
   }
 }
 
 export default SignInWidget;
+
