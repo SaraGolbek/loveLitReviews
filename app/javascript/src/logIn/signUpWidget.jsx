4a77b3127598e80a31a1c6d30ddb17521894f041
SaraGolbek <145944090+SaraGolbek@users.noreply.github.com>
2024-03-08 13:04:11 -0800

    Update signUpWidget.jsx

--- app/javascript/src/logIn/signUpWidget.jsx
+++ app/javascript/src/logIn/signUpWidget.jsx
@@ -1,24 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-
 import './stylesheets/login';
 
 class SignUpWidget extends React.Component {
-
   render() {
-   return ( 
-     <React.Fragment>
-       <form className="sign-up">
-         <h6 className="mb-3">Sign Up</h6>
-         <input type="text" className="inputwidth email" id="newemail" placeholder="Email" ></input>
-         <input type="text" className="inputwidth username" id="newusername" placeholder="Username" ></input>
-         <input type="password" className="inputwidth password" id="newpass" placeholder="Password"></input>
-         <p>Have an Account? Sign In</p>
-         <button className="inputwidth" id="sign-up-btn">Sign Up</button>
-       </form>
-     </React.Fragment>
-     );
+    return (
+      <React.Fragment>
+        <form className="sign-up">
+          <h6 className="mb-3">Sign Up</h6>
+          <input type="text" className="inputwidth email" id="newemail" placeholder="Email"></input>
+          <input type="text" className="inputwidth username" id="newusername" placeholder="Username"></input>
+          <input type="password" className="inputwidth password" id="newpass" placeholder="Password"></input>
+          <p onClick={this.props.onSignInClick}>Have an Account? Sign In</p>
+          <button className="inputwidth" id="sign-up-btn">Sign Up</button>
+        </form>
+      </React.Fragment>
+    );
   }
 }
 
-export defualt SignUpWidget;
+export default SignUpWidget;
