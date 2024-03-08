39e1796ebdc5335f7621e08eee6f05045af92324
SaraGolbek <145944090+SaraGolbek@users.noreply.github.com>
2024-03-08 10:50:52 -0800

    Update signUpWidget.jsx

--- app/javascript/src/logIn/signUpWidget.jsx
+++ app/javascript/src/logIn/signUpWidget.jsx
@@ -1,7 +1,24 @@
- <form className="sign-up" onSubmit={handleSignUp}>
-  <h6 className="mb-3">New to Twitter?</h6>
-  <input type="text" className="inputwidth email" id="newemail" placeholder="Email" required onChange={(event) => setEmail(event.target.value)}></input>
-  <input type="text" className="inputwidth username" id="newusername" placeholder="Username" required onChange={(event) => setUsername(event.target.value)}></input>
-  <input type="password" className="inputwidth password" id="newpass" placeholder="Password" required onChange={(event) => setPassword(event.target.value)}></input>
-  <button className="inputwidth" id="sign-up-btn">Sign Up for Twitter</button>
-</form>
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './stylesheets/login';
+
+const SignUpWidget = () => {
+  
+ return ( 
+   <form className="sign-up" onSubmit={handleSignUp}>
+     <h6 className="mb-3">New to Twitter?</h6>
+     <input type="text" className="inputwidth email" id="newemail" placeholder="Email" required onChange={(event) => setEmail(event.target.value)}></input>
+     <input type="text" className="inputwidth username" id="newusername" placeholder="Username" required onChange={(event) => setUsername(event.target.value)}></input>
+     <input type="password" className="inputwidth password" id="newpass" placeholder="Password" required onChange={(event) => setPassword(event.target.value)}></input>
+     <button className="inputwidth" id="sign-up-btn">Sign Up for Twitter</button>
+   </form>
+   );
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  ReactDOM.render(
+    <SignUpWidget />,
+    document.body.appendChild(document.createElement('div')),
+  )
+})
