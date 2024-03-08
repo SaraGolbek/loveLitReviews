c257c8476454f1eb453eaf84b15607f9dfbd1bce
SaraGolbek <145944090+SaraGolbek@users.noreply.github.com>
2024-03-08 11:45:01 -0800

    Update signUpWidget.jsx

--- app/javascript/src/logIn/signUpWidget.jsx
+++ app/javascript/src/logIn/signUpWidget.jsx
@@ -8,12 +8,13 @@ class SignUpWidget extends React.Component {
   render() {
    return ( 
      <React.Fragment>
-       <form className="sign-up" onSubmit={handleSignUp}>
-         <h6 className="mb-3">New to Twitter?</h6>
-         <input type="text" className="inputwidth email" id="newemail" placeholder="Email" required onChange={(event) => setEmail(event.target.value)}></input>
-         <input type="text" className="inputwidth username" id="newusername" placeholder="Username" required onChange={(event) => setUsername(event.target.value)}></input>
-         <input type="password" className="inputwidth password" id="newpass" placeholder="Password" required onChange={(event) => setPassword(event.target.value)}></input>
-         <button className="inputwidth" id="sign-up-btn">Sign Up for Twitter</button>
+       <form className="sign-up">
+         <h6 className="mb-3">Sign Up</h6>
+         <input type="text" className="inputwidth email" id="newemail" placeholder="Email" ></input>
+         <input type="text" className="inputwidth username" id="newusername" placeholder="Username" ></input>
+         <input type="password" className="inputwidth password" id="newpass" placeholder="Password"></input>
+         <p>Have an Account? Sign In</p>
+         <button className="inputwidth" id="sign-up-btn">Sign Up</button>
        </form>
      </React.Fragment>
      );
