5409f1e6b1d0304df52c982df3a890426b76ba18
Sara <golbeksara@gmail.com>
2024-03-19 17:50:26 -0700

    sign in fixes

--- app/javascript/src/layout.jsx
+++ app/javascript/src/layout.jsx
@@ -30,6 +30,7 @@ const Layout = (props) => {
               <ul className="navbar-nav">
                 <li><a className="nav-link" href="/home">Reviews</a></li>
                 <li><a className="nav-link" href="/profile">Bookshelf</a></li>
+                <li><a className="nav-link" href="/loginPage" >Sign In</a></li>
                 <li><a className="nav-link" href="/loginPage" onClick={handleSignOut}>Sign Out</a></li>
               </ul>
             </div>
