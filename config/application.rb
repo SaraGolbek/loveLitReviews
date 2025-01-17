c332614631bed07615ab37f4cdbb8c75d6819380
Sara Perez (Golbek) <145944090+saragolbek@users.noreply.github.com>
2025-01-17 12:01:44 -0800

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -8,13 +8,14 @@ Bundler.require(*Rails.groups)
 
 module LoveLitReviews
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.1
+    # Initialize configuration defaults for Rails 7
+    config.load_defaults 7.0
 
     # Configure default headers to prevent caching
     config.action_dispatch.default_headers = {
       'Cache-Control' => 'no-store'
     }
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
