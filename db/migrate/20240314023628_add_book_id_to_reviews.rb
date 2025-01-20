88830556e739261d30864b8abad927088df2c1e7
Sara Perez (Golbek) <145944090+saragolbek@users.noreply.github.com>
2025-01-19 16:04:15 -0800

    Delete db directory

--- db/migrate/20240314023628_add_book_id_to_reviews.rb
+++
@@ -1,5 +0,0 @@
-class AddBookIdToReviews < ActiveRecord::Migration[6.1]
-  def change
-    add_column :reviews, :book_id, :string
-  end
-end
--- db/migrate/20240314205157_add_author_to_reviews.rb
+++
@@ -1,5 +0,0 @@
-class AddAuthorToReviews < ActiveRecord::Migration[6.1]
-  def change
-    add_column :reviews, :author, :string
-  end
-end
